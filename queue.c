ee9f1da9ab8f0420d38a3158a55193017a2b8158
DhruwalPorwal <71888961+DhruwalPorwal@users.noreply.github.com>
2020-10-01 00:49:28 +0530

    Create queue.c

---
+++ queue.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+int front=-1,rear=-1,element,queue[50],item,n,i;
+void insert()
+{
+	if(rear==n-1)
+                printf("Queue is Full");
+        else
+        {
+		if(front==-1)
+			front=0;
+                printf("Enter the element to be inserted: ");
+                scanf("%d",&item);
+		rear=rear+1;
+		queue[rear]=item;
+        }
+}
+
+void delete()
+{
+	if(front==-1 || front>rear)
+	{
+		printf("Queue is Empty. Insert an element first");
+	}
+	else
+	{
+		printf("Deleted element is: %d", queue[front]);
+		front=front+1;
+	}
+}
